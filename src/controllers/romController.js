4e35365702e42b2aa87300466811f19f416d26cf
Matheus Guerreiro <matheus.guerreiro.792@gmail.com>
2024-12-03 17:52:14 -0300

    feat: romController. '

--- src/controllers/romController.js
+++ src/controllers/romController.js
@@ -27,4 +27,55 @@ const createRom = async (req, res) => {
   }
 };
 
-module.exports = { getRoms, createRom };
+const getRom = async (req, res) => {
+  try {
+    const rom = await Rom.findByPk(req.params.id);
+    res.json(rom);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateRom = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, year, frontImages, backImages, downloadLink } =
+      req.body;
+    const rom = await Rom.findByPk(id);
+
+    if (!rom) {
+      return res.status(404).json({ message: "Rom not found" });
+    }
+
+    await rom.update({
+      title,
+      description,
+      year,
+      frontImages,
+      backImages,
+      downloadLink,
+    });
+
+    res.json(rom);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteRom = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const rom = await Rom.findByPk(id);
+
+    if (!rom) {
+      return res.status(404).json({ message: "Rom not found" });
+    }
+
+    await rom.destroy();
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getRoms, createRom, getRom, updateRom, deleteRom };
